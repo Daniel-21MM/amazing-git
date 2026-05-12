da712c3fe6e461f0b06b87afd9cdbaa324546fe7
Daniel Mendez <danielsanz8001@gmail.com>
2026-05-12 11:08:43 -0600

    Revert "Bucle infinito"
    This reverts commit 1cbaee2f6171a138f397bf2fa9759be00f3cb624.

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 const a = "Hello, World!";
-
-while (true) {
-  console.log("This will run indefinitely.");
-}
-
 console.log(a);
-
-// More code can be added here, but it will never be reached due to the infinite loop above.
